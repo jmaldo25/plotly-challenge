c078d4135a5c173f49d653590f2de225385afdfc
Josue Maldonado <joshmaldo@Josues-MacBook-Pro.local>
2021-07-12 22:56:01 -0500

    created bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -13,4 +13,32 @@ function dropDown(sampleData){
         newOption.property('value', name) });
 };
 
+// Set data to new selection
+function optionChanged(selected){
+    makeBar(selected);
+    makeBubble(selected);
+    makeTable(selected);
+    makeGauge(selected);
+};
 
+// Creating Bar Chart of data
+function makeBar(sample){
+    //console.log(sample);   // Test to see if info was read in correctly
+    d3.json(filePath).then(function(data){
+        var samples = data['samples'];
+        var selectedSamples = samples.filter(bio=>bio['id'] ==sample) [0];
+        //console.log(currentSample)  // Test for info being read
+        var traceBar={
+            x: selectedSamples['sample_values'].slice(0,10),
+            y: selectedSamples['otu_ids'].map(otu_id=>'OTU ' +otu_id).slice(0,10),
+            type: 'bar',
+            text: selectedSamples['otu_labels'].slice(0,10),
+            orientation: 'h'
+        };
+        var layout ={
+            title: "Microbe Abundance",
+            xaxis: { title: "Abundance" },
+        };
+        Plotly.newPlot('bar', [traceBar], layout);
+    });
+};
